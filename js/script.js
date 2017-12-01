82e513bf12f12d07998e27fe4a8a305a8701870b
Rob | Labs <roblabs@users.noreply.github.com>
2017-12-01 11:39:12 -0800

    tokens are for access

--- js/script.js
+++ js/script.js
@@ -21,7 +21,32 @@
  * THE SOFTWARE.
  */
 
-mapboxgl.accessToken = '';
+ (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+ var Canvas = require('canvas-browserify')
+
+
+ // browserify canvas-browserify.node > js/canvas.js
+
+ },{"canvas-browserify":2}],2:[function(require,module,exports){
+
+ var Canvas = module.exports = function Canvas (w, h) {
+   var canvas = document.createElement('canvas')
+   canvas.width = w || 300
+   canvas.height = h || 150
+   return canvas
+ }
+
+ Canvas.Image = function () {
+   var img = document.createElement('img')
+   return img
+ }
+
+
+
+
+ },{}]},{},[1]);
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9ibGFicyIsImEiOiJjamFsZDl4ZXgycjljMzFucTI3cXBhOGluIn0.nDnS_YSbkFLICDf6qmvJGA';
 
 var form = document.getElementById('config');
 
